65fa7be85b80c4441b27a05835b987920c280699
AyresBarbara <Ayresbarbara2@gmail.com>
2025-05-21 14:49:08 -0300

    Add botão de favoritos e exibição dos favoritos

--- ionicnewsapp/climaApp/src/app/home/home.page.ts
+++ ionicnewsapp/climaApp/src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
+import { FavoritosService } from '../services/favoritos.service';
+
+
 
 @Component({
   selector: 'app-home',
@@ -9,14 +12,20 @@ import { WeatherService } from '../services/weather.service';
 })
 export class HomePage {
 
-  city = '';
+  city = 'Recife';
   weatherData: any;
   forecastData: any;
+  favoritos: string[] = [];
+  favoritosClima: { cidade: string, clima: any }[] = [];
 
-  constructor(private weatherService: WeatherService) {}
+  constructor(
+    private weatherService: WeatherService,
+    private favoritosService: FavoritosService
+  ) {}
 
   ngOnInit() {
     this.getWeather();
+    this.listarFavoritos();
   }
 
   getWeather() {
@@ -27,6 +36,31 @@ export class HomePage {
     this.weatherService.getForecastByCity(this.city).subscribe(data => {
       this.forecastData = data;
     });
+  }
+  
+  async adicionarFavorito() {
+    await this.favoritosService.adicionarFavorito(this.city);
+    this.listarFavoritos();
+  }
+
+  async listarFavoritos() {
+    this.favoritos = await this.favoritosService.getFavoritos();
+    this.favoritosClima = [];
+
+  for (const cidade of this.favoritos) {
+    this.weatherService.getWeatherByCity(cidade).subscribe(data => {
+      this.favoritosClima.push({ cidade, clima: data });
+    });
+  }
+  }
+
+  async removerFavorito(cidade: string) {
+    await this.favoritosService.removerFavorito(cidade);
+    this.listarFavoritos();
+  }
+  
+  async verificarFavorito(): Promise<boolean> {
+    return await this.favoritosService.isFavorito(this.city);
   }
 
 }
